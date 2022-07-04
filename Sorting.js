2f9b4af627a643481f80db417dff9bccafd4ed34
SeaN0X <69431500+SeaN0X@users.noreply.github.com>
2022-07-04 12:51:23 -0300

    Add files via upload

---
+++ Sorting.js
@@ -0,0 +1,32 @@
+function sorting(arg) {
+var rem = 0
+
+function sort(index){
+    oldarr=arg
+    var firstnum = Number(arg[index])
+    var secondnumplus = Number(arg[index+1])
+    if(firstnum > secondnumplus) {
+        arg[index] = secondnumplus
+        arg[index+1] = firstnum
+    } else {rem+=1;}
+}
+
+function sortprocess(){
+	rem = 0
+	for (i = 0;i<arg.length-1;i++) {
+		   sort(i)
+	}
+}
+
+var loop = setInterval(function(){
+	if(rem==arg.length-1){
+		return output
+		clearInterval(loop)
+	} else {
+		sortprocess()
+	}
+})
+}
+
+
+// exemplo: sorting([1, 5, 7, 9, 8, 4, 3, 10, 2, 6])
